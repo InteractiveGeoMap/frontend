8b792c87b2ca368625996412c79bd32912335bba
Yim Jiun Jye <jiunjye.yim@gmail.com>
2024-08-24 14:42:02 +0800

    Chore: cleaning comments

--- src/components/GeoMap/RadioGroupToggle.tsx
+++ src/components/GeoMap/RadioGroupToggle.tsx
@@ -1,17 +1,12 @@
-import { Box, HStack, Button, useColorModeValue, useRadioGroup, useRadio } from "@chakra-ui/react";
-import React, { FC, ReactNode, Children, useState} from "react";
+import { Box, BoxProps, Button, useColorModeValue, useRadioGroup, useRadio, UseRadioProps, Center } from "@chakra-ui/react";
+import React, { FC } from "react";
 
-// interface RadioOption {
-//     options: String;
-//     // any props that come into the component
-// }
-
-interface RadioGroupOptions {
+interface RadioGroupOptions extends BoxProps {
   options?: String;
   onTab: React.Dispatch<React.SetStateAction<String>>;
 }
 
-function CustomRadio(props) {
+function CustomRadio(props:UseRadioProps) {
   const light = useColorModeValue("black", "white");
   const { ...radioProps } = props
   const { state, getInputProps, getRadioProps } =
@@ -25,6 +20,7 @@ function CustomRadio(props) {
           w={16}
           p={1}
           rounded='full'
+          variant='ghost'
           color={state.isChecked ? 'white' : light}
         >
           {radioProps.value}
@@ -35,29 +31,20 @@ function CustomRadio(props) {
 
 
 
-const RadioGroupToggle: FC<RadioGroupOptions>  = ({options, onTab}) => {
+const RadioGroupToggle: FC<RadioGroupOptions>  = ({w='20px', options="Insert options", onTab}) => {
   const bg = useColorModeValue("gray.200", "gray.600");
-  var toggleOptions;
-  if (typeof options !== "undefined"){
-    toggleOptions = options.split(",");
-  } else {
-    toggleOptions = ["Insert options"];
-  }
-
+  const toggleOptions = options.split(",")
   const handleChange = (value: String) => {
     onTab(value)
   }
 
-  const { value, getRadioProps, getRootProps } = useRadioGroup({
+  const { getRadioProps } = useRadioGroup({
     defaultValue: toggleOptions[0],
     onChange: handleChange
   })
-  // console.log("options: ", options)
-  // console.log("Toggle options: ", toggleOptions)
-  // console.log("Value: ", value)
   return(
-    <Box bg={bg} rounded='full' p='0.5rem'>
-      <HStack>
+    <Box bg={bg} rounded='full' p='0.5rem' w={w}>
+      <Center>
         {toggleOptions.map((option) => {
             return (
               <CustomRadio
@@ -66,7 +53,7 @@ const RadioGroupToggle: FC<RadioGroupOptions>  = ({options, onTab}) => {
               />
             )
           })}
-      </HStack>
+      </Center>
     </Box>
   )
 };
